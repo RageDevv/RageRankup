be6e5564084b387483e9354e6820705f95732a29
Rage <84096429+RageDevv@users.noreply.github.com>
2021-05-13 09:16:32 -0400

    Update Main.php

--- src/rage/Main.php
+++ src/rage/Main.php
@@ -3,9 +3,13 @@
 namespace rage;
 
 use jojoe77777\FormAPI\SimpleForm;
+use onebone\EconomyAPI;
+use pocketmine\math\Vector3;
+use pocketmine\level\Position;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 
 
@@ -25,15 +29,19 @@ class Main extends PluginBase implements Listener{
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
     {
         $command = strtolower($command);
-        if($command == "heal"){
-            if($sender->hasPermission("use.heal"));
-            $this->form($sender);
+        if($command == "rankup"){
+        	if($sender instanceof Player){
+				$this->form($sender);
+			} else {
+        		$sender->sendMessage("hey you cant do this from console!");
+			}
+
         }
         return true;
     }
 
     public function form($player){
-        $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+        $api = $this->getServer()->getPluginManager()->getPlugins("FormAPI");
         $form = $api->createSimpleForm(function (Player $player, int $data = null){
             $result = $data;
             if($result === null){
@@ -42,16 +50,23 @@ class Main extends PluginBase implements Listener{
             switch ($result){
 
                 case 0:
-                    $player->setHealth($player->getMaxHealth());
-                    $player->sendMessage("Healed to the max");
+
+                	EconomyAPI::getInstance()->removeMoney($player, 50);
+                	$player->sendMessage("Rank Has been Upgraded!");
+                	$player->teleport(new Vector3(10, 60, 50));
                     break;
+				case 1:
+
+					EconomyAPI::getInstance()->addMoney($player, 100);
+					$player->sendMessage("You have now gained 100 dollars!");
 
             }
             return false;
         });
-        $form->setTitle("Heal Form");
-        $form->setContent("this is a form to heal your self!");
-        $form->addButton("Heal");
+        $form->setTitle("RankUp Form");
+        $form->setContent("this is a form to rank up!");
+        $form->addButton("Mine A (Price $50)");
+		$form->addButton("Get 100 dollars!");
         $form->sendToPlayer($player);
         return $form;
     }
